8c3d84e8892c98ac5f2f2c37e8f10443ea54d4a5
Asfiya Rashid Chowdhury <chowdhuryasfiya01@gmail.com>
2025-04-09 22:48:35 +0600

    registration

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,24 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\UniversityController;
-
+use App\Http\Controllers\UniRegistrationController;
 
 // Homepage Route
 Route::get('/', function () {
     return view('homepage');
 })->name('homepage');
 
-Route ::get('login', [AuthController::class, 'login'])->name('login');
+// Authentication Routes
+Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'loginPost'])->name('login.post');
 
-Route::get('homepage/registration', [AuthController::class, 'registration'])->name('univeersity.registration');
-Route::post('homepage/registration', [AuthController::class, 'registrationPost'])->name('university.registration.post');
 
+Route::get('/university-registration', [UniRegistrationController::class, 'showRegistrationForm'])->name('university.registration');
+Route::post('/university-registration', [UniRegistrationController::class, 'register'])->name('university.registration.post');
 
-Route::get('/universities/accredited', [UniversityController::class, 'showAccredited'])->name('universities.accredited');
-
-// About Page Route (using controller)
+// About Page Route
 Route::get('/about', [AboutController::class, 'index'])->name('about');
+
+// University Controller Routes
+Route::get('/universities/accredited', [UniversityController::class, 'showAccredited'])->name('universities.accredited');
